6525cd9ba55f0e8c9b4148d03d842bcef21481f5
Tuan Anh <langthambca@gmail.com>
2022-11-14 16:41:57 +0700

    route basic

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './custom.css'
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Note from './components/Note';
+import { useSelector } from 'react-redux';
 
-export default class App extends Component {
-  static displayName = App.name;
 
-  render () {
-    return (
-     <>
-     <Note/>
-     </>
-    );
-  }
+
+export default function App() {
+  const getUrlRedux =  useSelector((state)=> state.note.checkURL)
+  return (
+    <>
+      <Routes>
+        {/* <Route path="/" element={   <Navigate to={`${getUrlRedux == null ? '/' : getUrlRedux.url}`}  />} />
+        <Route path="/:url" element={<Note />} /> */}
+        <Route path="/" element={<Note />} />
+      </Routes>
+    </>
+
+
+  );
 }
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -5,13 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { store } from './app/store'
+
 import { Provider } from 'react-redux'
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
